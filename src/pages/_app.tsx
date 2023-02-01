26e0967ca4892438dd9909af21bf5fbfd79013d5
andreramosdovale <andre_ramosdovale@outlook.com>
2023-02-01 14:51:18 -0300

    feat: start project

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,12 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '@/styles/globals.css';
+import { ChakraProvider } from '@chakra-ui/react';
+import type { AppProps } from 'next/app';
+import { deafaultTheme } from '@/styles/theme';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider theme={deafaultTheme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,16 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document';
+import { ColorModeScript } from '@chakra-ui/react';
+import { deafaultTheme } from '@/styles/theme';
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
       <body>
+        <ColorModeScript initialColorMode={deafaultTheme.initialColorMode} />
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
